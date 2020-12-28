440546c063ef5c9a2385b59977d8d7c30906bb38
emcmunsayac@gmail.com <33993580+CoconutMartin@users.noreply.github.com>
2020-12-29 01:13:26 +0800

    First commit

---
+++ R/PSEIpullr.R
@@ -0,0 +1,25 @@
+#' Pull stock prices via pselookup API
+#'
+#' A function to full single/multiple stock prices listed in the Philippine Stock Exchange via pselookup API
+#' @param get_historical_price function to pull stock prices
+#' @param ticker stock ticker to pull
+#' @param type type of price to pull
+#' @param start_date start date of data pull
+#' @param end_date end date of data pull
+#' @return A data frame of stock prices
+#'
+#' @export
+
+
+######## Setup function for pulling historical price
+get_historical_price <- function(ticker = "x", type = c("open", "high", "low", "close"), start_date = "yyyy-mm-dd", end_date = "yyyy-mm-dd") {
+
+  requireNamespace(c("dplyr", "lubridate", "purrr", "tidyr", "tidyverse"))
+  price.json <- httr::GET(paste("https://pselookup.vrymel.com/api/stocks", ticker, "history", start_date = start_date, end_date = end_date, sep = "/"))
+  price <- jsonlite::fromJSON(rawToChar(price.json$content))$history
+  price <- price[, c("trading_date", type)]
+  colnames(price) <- c("Date", ticker)
+  price$Date <- lubridate::ymd(price$Date)
+
+  return(price)
+}
---
+++ README.md
@@ -0,0 +1,44 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# PSEIpullr
+
+<!-- badges: start -->
+
+<!-- badges: end -->
+
+PSEIpullr is used to pull prices of stocks listed in the Philippine
+Stock Exchange via the pselookup API.
+
+## Installation
+
+You can install the released version of PSEIpullr from
+[CRAN](https://CRAN.R-project.org) with:
+
+``` r
+install.packages("PSEIpullr")
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(PSEIpullr)
+
+system.time(
+  ac <- get_historical_price(ticker = "AC", type = "close", start_date = "2020-01-01", end_date = "2020-02-01")
+)
+#> Loading required namespace: dplyr
+#>    user  system elapsed 
+#>    0.57    0.07    2.11
+
+head(ac)
+#>         Date    AC
+#> 1 2020-01-02 770.0
+#> 2 2020-01-03 776.0
+#> 3 2020-01-06 785.0
+#> 4 2020-01-07 800.0
+#> 5 2020-01-08 790.0
+#> 6 2020-01-09 794.5
+```
